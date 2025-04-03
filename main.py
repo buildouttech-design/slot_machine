58b17e3fafd288d77628a8b1660671b5c052e39d
Paul Anthony McGowan <buildouttechno@gmail.com>
2025-04-03 21:05:49 +0100

    get_number_of_lines

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+MAX_LINES = 3
 def deposit():
+    print("Welcome to the ATM!")
     while True:
         try:
             amount = float(input("Enter amount to deposit: "))
@@ -9,5 +11,29 @@ def deposit():
         except ValueError:
             print("Invalid input. Please enter a number.")
     print(f"You have deposited: {amount}")
+    return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input(
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+        
+
+def main():
+   balance = deposit()
+   lines = get_number_of_lines()
+   print(f"You have chosen to bet {balance} on {lines} lines.")
+    
+
+
+main()
